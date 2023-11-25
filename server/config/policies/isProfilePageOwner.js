4c2fb0da0d964f7d7479aa0cc431e393c3b9258c
Exanite <exanite78012@gmail.com>
2023-11-25 17:25:32 -0500

    Ensure profiles are accessed using (userId, isStudent) combination, rather than by profile ID

--- server/config/policies/isProfilePageOwner.js
+++ server/config/policies/isProfilePageOwner.js
@@ -10,18 +10,35 @@ module.exports = async (ctx, next) => {
 
   const isStudentParam = Boolean(JSON.parse(ctx.request.query.student ?? "false"));
   const idParam = Number(ctx.params.id);
+  const useUserId = Number(JSON.parse(ctx.request.query.useUserId ?? "false"));
+
+  console.log(Object.keys(ctx));
+
+  if (!useUserId) {
+    // Profiles can be accessed by profile ID OR by the combination of user id and isStudent.
+    // Only the second approach is supported right now.
+    ctx.badRequest("Accessing profiles currently only supports accessing by user ID. Please include the 'useUserId=true' query parameter in your request.");
+
+    return;
+  }
 
   if (ctx.state.user.isStudent) {
     const ids = ctx.state.user.ids;
     if (isStudentParam && ids.includes(idParam)) {
+      const profile = await strapi.services.profile.tryFindOrCreate(idParam, isStudentParam);
+      ctx.params.id = profile.id;
+
       return await next();
     }
   } else {
     const id = ctx.state.user.id;
     if (!isStudentParam && id == idParam) {
+      const profile = await strapi.services.profile.tryFindOrCreate(idParam, isStudentParam);
+      ctx.params.id = profile.id;
+
       return await next();
     }
   }
 
-  ctx.unauthorized(`You can only modify your own profile!`)
+  ctx.unauthorized(`The requested profile is not your profile.`)
 }
