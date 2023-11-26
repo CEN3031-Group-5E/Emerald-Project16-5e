2dcaae4d77d6d3dada541326e4ec58975cfa3adc
josht8601 <josht8601@gmail.com>
2023-11-26 17:35:03 -0500

    updated badge display and connected progress bars to badges

--- client/src/components/Profile/BadgeDisplay.jsx
+++ client/src/components/Profile/BadgeDisplay.jsx
@@ -1,15 +1,18 @@
 import "./BadgeDisplay.less";
 import Badge from "./Badge";
 
-const BadgeDisplay = () => {
+const BadgeDisplay = ({ imageUrl, name, progressPercent}) => {
   return (
     <div class='badge-display-container'>
         <div class='badge-display-slot badge-display-item-border badge-display-round-large'>
             <Badge 
-                imageUrl={"https://media.discordapp.net/attachments/517010400860962831/1171160597463838840/image.png"}
-                name={"Complete 100 Python exercises"}
-                progressPercent={99} 
+                //imageUrl={"https://media.discordapp.net/attachments/517010400860962831/1171160597463838840/image.png"}
+                //name={"Complete 100 Python exercises"}
+                imageUrl = {imageUrl}
+                name = {name}
+                progressPercent={progressPercent} 
             />
+
             
         </div>
         <h3 class='profile-badge-display-change-button badge-display-round-large badge-display-item-border'>Change</h3>
--- client/src/components/Profile/BadgeTable.jsx
+++ client/src/components/Profile/BadgeTable.jsx
@@ -1,31 +1,57 @@
-import React from 'react';
-import Badge from './Badge';
-import './BadgeTable.less';
+  import React from 'react';
+  import Badge from './Badge';
+  import './BadgeTable.less';
+  import ProgressBar from './ProgressBar';
 
-const BadgeTable = () => {
-  const numRows = 4;
-  const numColumns = 4;
+  // Component to display a table of badges
+  const BadgeTable = ({ badgeProgress }) => {
+    const inProgressBadges = badgeProgress.filter(badge => badge.progress < 100);
 
-  const data = Array.from({ length: numRows }, () =>
-    Array.from({ length: numColumns }, () => 'Badge')
-  );
+    // Define the number of rows and columns for the badge table
+   // const numRows = 4;
+    //const numColumns = 4;
+    const numRows = 4; 
+    const numColumns = Math.ceil(inProgressBadges.length / numRows);
+    
 
-  return (
-    <div className="grid-table">
-      {data.map((row, rowIndex) =>
-        row.map((cell, cellIndex) => (
-          <div className="grid-cell " key={`cell-${rowIndex}-${cellIndex}`}>
-            {/* TODO change hardcoded badge prop values to cell data values */}
-            <Badge 
-                imageUrl={"https://media.discordapp.net/attachments/517010400860962831/1171160597463838840/image.png"}
-                name={"Complete 100 Python exercises"}
-                progressPercent={99}  
-            />
-          </div>
-        ))
-      )}
-    </div>
-  );
-};
+    // Create a 2D array to represent the badge data in rows and columns
+    const data = Array.from({ length: numRows }, () =>
+      Array.from({ length: numColumns }, () => 'Badge')
+    );
 
-export default BadgeTable;
+    return (
+      // Render the grid table
+      <div className="grid-table">
+        {/*{data.map((row, rowIndex) =>
+          row.map((cell, cellIndex) => (
+            <div className="grid-cell " key={`cell-${rowIndex}-${cellIndex}`}>
+              <Badge 
+                  imageUrl={"https://media.discordapp.net/attachments/517010400860962831/1171160597463838840/image.png"}
+                  name={"Complete 100 Python exercises"}
+                  progressPercent={100}  
+              />
+            </div>
+          ))
+        )}*/}
+        {Array.from({ length: numRows }, (_, rowIndex) =>
+          Array.from({ length: numColumns }, (_, colIndex) => {
+            const badgeIndex = rowIndex * numColumns + colIndex;
+            const badge = inProgressBadges[badgeIndex];
+
+            return badge ? (
+              <div className="grid-cell" key={`cell-${rowIndex}-${colIndex}`}>
+                <Badge 
+                  imageUrl={badge.imageUrl}
+                  name={badge.name}
+                  progressPercent={badge.progress}  
+                />
+                <ProgressBar progress={badge.progress} />
+              </div>
+            ) : null;
+          })
+        )}
+      </div>
+    );
+  };
+
+  export default BadgeTable;
