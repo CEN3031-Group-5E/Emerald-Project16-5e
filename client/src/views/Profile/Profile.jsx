8d9e2e2911b591fb97f09513306c19b7743180bc
Rohit Nair <rnair122002@gmail.com>
2023-11-27 20:14:22 -0500

    Changed permissions for student vs teacher login

--- client/src/views/Profile/Profile.jsx
+++ client/src/views/Profile/Profile.jsx
@@ -22,8 +22,15 @@ const Profile = () => {
 
   if (!userId) {
     // Parameters aren't provided, use current user
-    userId = String(loggedInUser.id);
-    isStudent = Boolean(loggedInUser.isStudent);
+    isStudent = Array.isArray(loggedInUser)
+    if (isStudent) // Replace condition
+    {
+      userId = String(loggedInUser[0]);
+    }
+    else
+    {
+      userId = String(loggedInUser.id);
+    }
   }
 
   const [pageData, setPageData] = useState({
@@ -72,16 +79,29 @@ const Profile = () => {
 
   const [selectedBadges, setSelectedBadges] = useState([1, 2, 3, 4]);
   //const [isEditingBadges, setIsEditingBadges] = useState(null);
-  
+  const [badgeProgress, setBadgeProgress] = useState([
+    { id: 1, progress: 100, name: "1 Month", imageUrl: "https://cdn.discordapp.com/attachments/1163871478014554142/1178351638906408991/1monthbadge.png?ex=6575d47b&is=65635f7b&hm=78777dc5a28d30c57f9226676ea84f9786718b3911207216789be238e8e1b94d&" },
+    { id: 2, progress: 33.33, name: "3 Month", imageUrl: "https://cdn.discordapp.com/attachments/1163871478014554142/1178351637799129108/3monthbadge.png?ex=6575d47b&is=65635f7b&hm=b98a59f95dfa3f8d5578fcf27f9d7e4766a0cd6ed9722de0592eeda54cb52f93&" },
+    { id: 3, progress: 16.67, name: "6 Month", imageUrl: "https://cdn.discordapp.com/attachments/1163871478014554142/1178351637404856330/6monthbadge.png?ex=6575d47b&is=65635f7b&hm=a1d8c52aee335fdabba01dbab3164884aeb00f90a2d1904a0a9ce1ce2425a82a&" },
+    { id: 4, progress: 8.33, name: "1 Year", imageUrl: "https://cdn.discordapp.com/attachments/1163871478014554142/1178351637119635528/1yearbadge.png?ex=6575d47b&is=65635f7b&hm=39c8a82d57730cf9b53140a332b9dfad2027921c37ff19b36b30373f5ea6f945&" },
+    { id: 5, progress: 4.167, name: "2 Year", imageUrl: "https://cdn.discordapp.com/attachments/1163871478014554142/1178351636851216425/2yearbadge.png?ex=6575d47a&is=65635f7a&hm=92f19b1f5400a3b8b539d5c05262601f7d51d34c3a2cc3b9c6223def0e3ff804&" },
+    { id: 6, progress: 2.78, name: "3+ Years", imageUrl: "https://cdn.discordapp.com/attachments/1163871478014554142/1178351636565995520/3yearbadge.png?ex=6575d47a&is=65635f7a&hm=e08d34ac60519331baa525a21d65ea761d6a55319569965178d46fd963d7e4c3&" },
+    { id: 7, progress: 100, name: "50 Likes/Views", imageUrl: "https://cdn.discordapp.com/attachments/1163871478014554142/1178372577803116614/50likesbadge.png?ex=6575e7fb&is=656372fb&hm=0efd9b56e2bbf25977b6a6cc0147190403ba43717ad64a398ede7719065e4877&" },
+    { id: 8, progress: 50, name: "100 Likes/Views", imageUrl: "https://cdn.discordapp.com/attachments/1163871478014554142/1178372577450790953/100likesbadge.png?ex=6575e7fb&is=656372fb&hm=9258818704392822315c9cee9c725ec0e0d3d9551c50635b7e9b36a382e49314&" },
+    { id: 9, progress: 20, name: "250 Likes/Views", imageUrl: "https://cdn.discordapp.com/attachments/1163871478014554142/1178372577085898862/250likesbadge.png?ex=6575e7fb&is=656372fb&hm=35067678501562adf8f53d34e28934130338ce31de8474a1aba1cc58d7a52d89&" },
+    { id: 10, progress: 10, name: "500 Likes/Views", imageUrl: "https://cdn.discordapp.com/attachments/1163871478014554142/1178372576737759293/500likebadge.png?ex=6575e7fb&is=656372fb&hm=7881b6ef5569f0607b3dbd5e6568d2501b3159f75f0478b18c9ca960d2e7871c&" },
+    { id: 11, progress: 5, name: "1000 Likes/Views", imageUrl: "https://cdn.discordapp.com/attachments/1163871478014554142/1178372576104423484/1000likesbadge.png?ex=6575e7fb&is=656372fb&hm=4ad75f9b3956340cd0bdd84a607f7f552699fe45ffb79c0764a85a4f4aca18ad&" },
+
+  ]);
   const [isEditingBadges, setIsEditingBadges] = useState(null);
 
   const renderBadgeDisplays = () => {
-    return pageData.badges
-      ?.filter(badge => badge.progress < 100)
+    return badgeProgress
+      .filter(badge => badge.progress < 100)
       .map(badge => (
         <BadgeDisplay
           key={badge.id}
-          imageUrl={`${server}${badge.image?.url}`}
+          imageUrl={badge.imageUrl}
           name={badge.name}
           progressPercent={badge.progress}
         />
@@ -89,12 +109,12 @@ const Profile = () => {
   };
 
   const renderFullyAchievedBadges = () => {
-    return pageData.badges
-      ?.filter(badge => badge.progress === 100)
+    return badgeProgress
+      .filter(badge => badge.progress === 100)
       .map(badge => (
         <BadgeDisplay
           key={badge.id}
-          imageUrl={`${server}${badge.image?.url}`}
+          imageUrl={badge.imageUrl}
           name={badge.name}
           progressPercent={badge.progress}
         />
